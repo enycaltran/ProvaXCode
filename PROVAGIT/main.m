696f5d770614c25d7a8fbc13fd1db387361349bd
Enrico Caltran <enrico@MacBook-Pro-di-Enrico.local>
2014-05-01 18:01:06 +0200

    Initial Commit

---
+++ PROVAGIT/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PROVAGIT
+//
+//  Created by Enrico Caltran on 01/05/14.
+//  Copyright (c) 2014 Enrico Caltran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
